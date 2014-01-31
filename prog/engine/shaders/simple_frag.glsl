2fa6c6fc2246bab8b1efa146079018e458c9d84d
monkeybits <kristianselvaag@gmail.com>
2014-01-31 19:30:02 +0100

    Added valid UV-coordinates for sphere model, took an additional texture in use and turned off light attenuation in the fragment shader to make the scene a little bit brighter.

--- prog/engine/shaders/simple_frag.glsl
+++ prog/engine/shaders/simple_frag.glsl
@@ -69,8 +69,8 @@ void main (void)
                 //light_cols_att = light_cols[i]*1/(pow(length(mypos - position),1)); // attenuate
 //                light_cols_att = vec4(1.0, 1.0, 1.0, 1.0);
                 light_cols_att = light_cols[i];
-                float light_dist = ((light_posns[0].x - mypos.x) * (light_posns[0].x - mypos.x) + (light_posns[0].y  - mypos.y)* (light_posns[0].y  - mypos.y)+ (light_posns[0].z  - mypos.z)* (light_posns[0].z - mypos.z));
-                light_cols_att = vec4(light_cols[i].r/light_dist, light_cols[i].g/light_dist, light_cols[i].b/light_dist, 1.0);
+                // float light_dist = ((light_posns[0].x - mypos.x) * (light_posns[0].x - mypos.x) + (light_posns[0].y  - mypos.y)* (light_posns[0].y  - mypos.y)+ (light_posns[0].z  - mypos.z)* (light_posns[0].z - mypos.z));
+                // light_cols_att = vec4(light_cols[i].r/light_dist, light_cols[i].g/light_dist, light_cols[i].b/light_dist, 1.0);
                 lightdirection = normalize (position - mypos);
             }
             else
