c0f1478fba90aad6e35b63daee21030ea7e9c233
monkeybits <kristianselvaag@gmail.com>
2016-02-27 13:52:00 +0100

    clean scoping in event loop

--- prog/engine/src/master.cpp
+++ prog/engine/src/master.cpp
@@ -40,15 +40,15 @@ Master::Master() :
         mechanics.init(world, dt);
 
         // do other multi_threading things;
-        back_ground = Thread(&Master::backGroundTasks, this);
-        //backGroundTasks(); // serially
+        // back_ground = Thread(&Master::backGroundTasks, this);
+        backGroundTasks(); // serially
 
         // enter the main loop
         mainLoopSingleThreaded();
 
         // this point is reached when an exit signal has been received
         // join background threads
-        back_ground.join(); //
+        // back_ground.join(); //
 
 
     }
@@ -284,21 +284,21 @@ void Master::cleanUp()
 
 bool Master::handleInput()
 {
-    // Process fast input
-    if (has_focus)
-    {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))      mechanics.playerMoveForward();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))      mechanics.playerMoveBackward();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))      mechanics.playerMoveLeft();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))      mechanics.playerMoveRight();
+//    // Process fast input
+//    if (has_focus)
+//    {
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))      mechanics.playerMoveForward();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))      mechanics.playerMoveBackward();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))      mechanics.playerMoveLeft();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))      mechanics.playerMoveRight();
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))     mechanics.playerRotateUp();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))   mechanics.playerRotateDown();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))   mechanics.playerRotateLeft();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  mechanics.playerRotateRight();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))     mechanics.playerRotateUp();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))   mechanics.playerRotateDown();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))   mechanics.playerRotateLeft();
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  mechanics.playerRotateRight();
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))  mechanics.playerShift();
-    }
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))  mechanics.playerShift();
+//    }
 
     // Slow event based input
     bool running = true;
@@ -306,159 +306,253 @@ bool Master::handleInput()
     sf::Event event;
     while (window.pollEvent(event))
     {
-        switch (event.type)
+        // "close requested" event: we close the window
+        if (event.type == sf::Event::Closed)
         {
-        case sf::Event::Closed:
+            window.close();
             running = false;
-            break;
-        case sf::Event::Resized:
-            // adjust the viewport when the window is resized
-            renderer.resizeWindow(event.size.width, event.size.height);
-            break;
-        case sf::Event::LostFocus:
-        {
-            has_focus = false;
-            window.setMouseCursorVisible (true);
-
-#ifdef WINDOWS
-            sf::WindowHandle window_handle = window.getSystemHandle(); // still portable
-
-
-            //running = false;
-            ShowWindow(window_handle, SW_MINIMIZE);
-#endif // WINDOWS
-
-            sf::Event focusevent;
-            while (!has_focus)
-            {
-                while (window.pollEvent(focusevent))
-                {
-                    if (focusevent.type == sf::Event::GainedFocus)
-                    {
-                        has_focus = true;
-                        window.setMouseCursorVisible (false);
-                        resetPressPos();
-                        // pause music/sound
-                    }
-                }
-            }
-        } break;
-        case sf::Event::GainedFocus: // Redundant?
-            has_focus = true;
-            window.setMouseCursorVisible (false);
-            resetPressPos();
-            break;
-        case sf::Event::MouseButtonPressed:
-            switch (event.mouseButton.button)
-            {
-            case sf::Mouse::Right:
-                {
-//                    window.setMouseCursorVisible (false);
-//                    sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-//                    press_pos_x = mouse_pos.x;
-//                    press_pos_y = mouse_pos.y;
-//                    rmb_down = true;
-                    mechanics.playerBlock();
-                } break;
-            case sf::Mouse::Middle: break;
-            default: break;
-            } break;
-        case sf::Event::MouseButtonReleased:
-            switch (event.mouseButton.button)
-            {
-            case sf::Mouse::Right:
-                {
-//                    window.setMouseCursorVisible (true);            // SELF
-//                    rmb_down = false;
-                } break;
-            case sf::Mouse::Left:
-                {
-                    mechanics.playerAttack();
-                } break;
-            case sf::Mouse::Middle: break;
-            default: break;
-            } break;
-        case sf::Event::KeyPressed:
-            switch (event.key.code)
-            {
-            case sf::Keyboard::Escape:
-                running = false;
-                m_restart = 0;
-                break;
-            case sf::Keyboard::R:
-                // do something
-                break;
-            case sf::Keyboard::E:
-                //mechanics.playerBlock();
-                break;
-            case sf::Keyboard::Q:
-                mechanics.playerDodge();
-                break;
-            case sf::Keyboard::Space:
-                mechanics.playerJump();
-                break;
-            case sf::Keyboard::Tab:
-                mechanics.playerStance();
-                break;
-            case sf::Keyboard::F1:
-                mechanics.func(1);
-                break;
-            case sf::Keyboard::F2:
-                mechanics.func(2);
-                break;
-            case sf::Keyboard::F3:
-                mechanics.func(3);
-                break;
-            case sf::Keyboard::F4:
-                mechanics.func(4);
-                break;
-            case sf::Keyboard::F5:
-                mechanics.func(5);
-                break;
-            case sf::Keyboard::F6:
-                mechanics.func(6);
-                break;
-            case sf::Keyboard::F7:
-                mechanics.func(7);
-                break;
-            case sf::Keyboard::F8:
-                mechanics.func(8);
-                break;
-            case sf::Keyboard::F9:
-                mechanics.func(9);
-                break;
-            case sf::Keyboard::F10:
-                mechanics.func(10);
-                break;
-            case sf::Keyboard::F11:
-                mechanics.func(11);
-                break;
-            case sf::Keyboard::F12:
-                mechanics.func(12);
-                break;
-            default:
-                break;
-            } // key pressed
-            break;
-        default:
-            break;
-        } // event type
-    } // while window.pollevent
-
-    if (has_focus)
-    {
-        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
-        int new_posx = mouse_pos.x;
-        int new_posy = mouse_pos.y;
-        int xoffset = new_posx - press_pos_x;
-        int yoffset = new_posy - press_pos_y;
-        //        cout<<" by "<<xoffset<<" and "<<yoffset;
-
-        mechanics.playerRotateLeftVal(-xoffset/4.0);
-        mechanics.playerRotateUpVal(-yoffset/4.0);
-
-        resetPressPos();
+        }
     }
+
+//    sf::Event event;
+//    while (window.pollEvent(event))
+//    {
+//        switch (event.type)
+//        {
+
+//            case sf::Event::Closed:
+//            {
+//                running = false;
+//                break;
+//            }
+
+//            case sf::Event::Resized:
+//            {
+//                // adjust the viewport when the window is resized
+//                renderer.resizeWindow(event.size.width, event.size.height);
+//                break;
+//            }
+
+//            case sf::Event::LostFocus:
+//            {
+//                has_focus = false;
+//                window.setMouseCursorVisible (true);
+
+//    #ifdef WINDOWS
+//                sf::WindowHandle window_handle = window.getSystemHandle(); // still portable
+
+
+//                //running = false;
+//                ShowWindow(window_handle, SW_MINIMIZE);
+//    #endif // WINDOWS
+
+//                sf::Event focusevent;
+//                while (!has_focus)
+//                {
+//                    while (window.pollEvent(focusevent))
+//                    {
+//                        if (focusevent.type == sf::Event::GainedFocus)
+//                        {
+//                            has_focus = true;
+//                            window.setMouseCursorVisible (false);
+//                            resetPressPos();
+//                            // pause music/sound
+//                        }
+//                    }
+//                }
+//                break;
+//            }
+
+//            case sf::Event::GainedFocus: // Redundant?
+//            {
+//                has_focus = true;
+//                window.setMouseCursorVisible (false);
+//                resetPressPos();
+//                break;
+//            }
+
+//            case sf::Event::MouseButtonPressed:
+//            {
+//                switch (event.mouseButton.button)
+//                {
+
+//                    case sf::Mouse::Right:
+//                    {
+////                      window.setMouseCursorVisible (false);
+////                      sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+////                      press_pos_x = mouse_pos.x;
+////                      press_pos_y = mouse_pos.y;
+////                      rmb_down = true;
+//                        mechanics.playerBlock();
+//                        break;
+//                    }
+
+//                    case sf::Mouse::Middle:
+//                    {
+//                        break;
+//                    }
+
+//                    default:
+//                    {
+//                        break;
+//                    }
+
+//                }
+
+//                break;
+//            }
+
+//            case sf::Event::MouseButtonReleased:
+//            {
+//                switch (event.mouseButton.button)
+//                {
+//                    case sf::Mouse::Right:
+//                    {
+//    //                    window.setMouseCursorVisible (true);            // SELF
+//    //                    rmb_down = false;
+//                        break;
+//                    }
+//                    case sf::Mouse::Left:
+//                    {
+//                        mechanics.playerAttack();
+//                        break;
+//                    }
+//                    case sf::Mouse::Middle:
+//                    {
+//                        break;
+//                    }
+//                    default:
+//                    {
+//                        break;
+//                    }
+//                }
+//                break;
+//            }
+
+//            case sf::Event::KeyPressed:
+//            {
+//                switch (event.key.code)
+//                {
+//                    case sf::Keyboard::Escape:
+//                    {
+//                        running = false;
+//                        m_restart = 0;
+//                        break;
+//                    }
+//                    case sf::Keyboard::R:
+//                    {
+//                        // do something
+//                        break;
+//                    }
+//                    case sf::Keyboard::E:
+//                    {
+//                        //mechanics.playerBlock();
+//                        break;
+//                    }
+//                    case sf::Keyboard::Q:
+//                    {
+//                        mechanics.playerDodge();
+//                        break;
+//                    }
+//                    case sf::Keyboard::Space:
+//                    {
+//                        mechanics.playerJump();
+//                        break;
+//                    }
+//                    case sf::Keyboard::Tab:
+//                    {
+//                        mechanics.playerStance();
+//                        break;
+//                    }
+//                    case sf::Keyboard::F1:
+//                    {
+//                        mechanics.func(1);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F2:
+//                    {
+//                        mechanics.func(2);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F3:
+//                    {
+//                        mechanics.func(3);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F4:
+//                    {
+//                        mechanics.func(4);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F5:
+//                    {
+//                        mechanics.func(5);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F6:
+//                    {
+//                        mechanics.func(6);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F7:
+//                    {
+//                        mechanics.func(7);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F8:
+//                    {
+//                        mechanics.func(8);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F9:
+//                    {
+//                        mechanics.func(9);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F10:
+//                    {
+//                        mechanics.func(10);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F11:
+//                    {
+//                        mechanics.func(11);
+//                        break;
+//                    }
+//                    case sf::Keyboard::F12:
+//                    {
+//                        mechanics.func(12);
+//                        break;
+//                    }
+//                    default:
+//                    {
+//                        break;
+//                    } // key pressed
+//                }
+//                break;
+//            }
+
+//            default:
+//            {
+//                break;
+//            }
+//        } // event type
+//    } // while window.pollevent
+
+//    if (has_focus)
+//    {
+//        sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+//        int new_posx = mouse_pos.x;
+//        int new_posy = mouse_pos.y;
+//        int xoffset = new_posx - press_pos_x;
+//        int yoffset = new_posy - press_pos_y;
+//        //        cout<<" by "<<xoffset<<" and "<<yoffset;
+
+//        mechanics.playerRotateLeftVal(-xoffset/4.0);
+//        mechanics.playerRotateUpVal(-yoffset/4.0);
+
+//        resetPressPos();
+//    }
 
     return running;
 }
